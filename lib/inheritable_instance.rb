357b982981cbafe35ef24b96998965b0f9fa224c
David Feldman <david.feldman@avant.com>
2016-05-25 19:17:35 -0500

    copy declared ivars during inheritance

--- lib/inheritable_instance.rb
+++ lib/inheritable_instance.rb
@@ -1,5 +1,28 @@
 require "inheritable_instance/version"
+require "inheritable_instance/safe_deep_dup"
+require "set"
 
 module InheritableInstance
-  # Your code goes here...
+
+  def inheritable_instance(ivar, value)
+    ivar = ivar.to_sym
+    inheritable_instance_vars << ivar
+    instance_variable_set ivar, value
+  end
+
+  def inherited(subclass)
+    super
+    inheritable_instance_vars.each do |ivar|
+      original  = instance_variable_get(ivar)
+      duplicate = SafeDeepDup.duplicate(original)
+      subclass.instance_variable_set(ivar, duplicate)
+    end
+  end
+
+  private
+
+  def inheritable_instance_vars
+    @inheritable_instance_vars ||= Set.new([:@inheritable_instance_vars])
+  end
+
 end
---
+++ lib/inheritable_instance/safe_deep_dup.rb
@@ -0,0 +1,42 @@
+require "bigdecimal"
+
+module InheritableInstance
+  module SafeDeepDup
+    extend self
+
+    SKIP = [ NilClass, FalseClass, TrueClass, Symbol, Module, Method ].freeze
+
+    def duplicate(obj)
+      return duplicate_hash(obj)  if obj.is_a?(Hash)
+      return duplicate_array(obj) if obj.is_a?(Array)
+      return obj.dup if should_duplicate?(obj)
+      obj
+    end
+
+    private
+
+    def should_duplicate?(obj)
+      return false if SKIP.any?{ |klass| obj.is_a?(klass) }
+      return false if obj.is_a?(Numeric) && !obj.is_a?(BigDecimal)
+      return false unless obj.respond_to?(:dup)
+      !obj.respond_to?(:duplicable?) || obj.duplicable?
+    end
+
+    def duplicate_array(array)
+      array.map{ |element| duplicate(element) }
+    end
+
+    def duplicate_hash(hash)
+      hash.each_with_object(blank_hash(hash)) do |(k, v), copy|
+        copy[duplicate(k)] = duplicate(v)
+      end
+    end
+
+    def blank_hash(hash)
+      return hash.dup.clear unless hash.class.equal?(Hash)
+      hash.default_proc ? Hash.new(&hash.default_proc) :
+                          Hash.new(hash.default)
+    end
+
+  end
+end
